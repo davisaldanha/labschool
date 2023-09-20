66c640201d1f7f798ae7febff699998f41c9fd44
Davi Saldanha <davisaldanha.ti@gmail.com>
2023-09-20 20:27:49 -0300

    Config Logging

--- src/main/java/com/example/labschool/LabschoolApplication.java
+++ src/main/java/com/example/labschool/LabschoolApplication.java
@@ -1,13 +1,19 @@
 package com.example.labschool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LabschoolApplication {
+    
+    private static Logger LOGGER = LoggerFactory.getLogger(LabschoolApplication.class);
 
 	public static void main(String[] args) {
+                LOGGER.info("INICIANDO API LABSCHOOL...");          
 		SpringApplication.run(LabschoolApplication.class, args);
+                LOGGER.info("API INICIADA COM SUCESSO.");
 	}
 
 }
